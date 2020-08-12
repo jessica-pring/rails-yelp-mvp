3b68c64896369c07b62cbafdb86ec43ed79b99b9
Jess Pring <jessicapring@outlook.com>
2020-08-12 14:01:42 +0100

    Restaurant and review models validating

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_111223) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
